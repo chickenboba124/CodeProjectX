55879bbf2ca40c25ffb6affb7ba2af2b4e74f917
alex hawley <alexhawley@alexs-Mac-mini.local>
2016-07-17 14:33:25 -0500

    2016-07-17 segment, webview, imageview, sizeclasses w/ font

---
+++ SwitchSegmentActvity/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SwitchSegmentActvity
+//
+//  Created by alex hawley on 7/17/16.
+//  Copyright © 2016 alex hawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myActivityController: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func segmentSwitched(sender: UISegmentedControl) {
+    
+        let index: Int = sender.selectedSegmentIndex;
+        let title: String = sender.titleForSegmentAtIndex(index)!
+        print("title is \(title)")
+    }
+    
+    @IBAction func switchChanged(sender: UISwitch) {
+    
+        if (sender.on == true) {
+            if (myActivityController.isAnimating() == false) {
+                myActivityController.startAnimating();
+            }
+        } else {
+            if (myActivityController.isAnimating() == true) {
+                myActivityController.stopAnimating();
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WebView/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by alex hawley on 7/17/16.
+//  Copyright © 2016 alex hawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myWebView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //let url: NSURLRequest = NSURLRequest(
+        
+        let url: NSURL = NSURL(string: "http://www.google.com")!
+        let urlRequest: NSURLRequest = NSURLRequest(URL: url)
+        
+        myWebView.loadRequest(urlRequest)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
