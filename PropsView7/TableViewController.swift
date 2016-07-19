3c667b07b6785a1a88146df14645bcf44120c9fd
alex hawley <alexhawley@alexs-Mac-mini.local>
2016-07-18 23:31:18 -0500

    2017-07-17 tableviews with json

---
+++ PropsView7/TableViewController.swift
@@ -0,0 +1,92 @@
+//
+//  TableViewController.swift
+//  PropsView7
+//
+//  Created by micheal kent on 7/18/16.
+//  Copyright © 2016 micheal kent. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var data:NSMutableData = NSMutableData()
+    var TableData:[String] = [];
+    @IBOutlet var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        get_data_from_url("http://alexhawley.net/properties/index.php")
+    }
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TableData.count;
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("propertyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = TableData[indexPath.row]
+        return cell
+    }
+    func get_data_from_url(url:String)
+    {
+        let url = NSURL(string: url)
+        var request: NSURLRequest = NSURLRequest(URL: url!)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        connection.start()
+    }
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        self.data = NSMutableData()
+    }
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        self.data.appendData(data)
+    }
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var err: NSError
+        do {
+            var json: AnyObject? = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)
+            if let properties_list = json as? NSArray {
+                for (var i = 0; i < properties_list.count ; i++ ) {
+                    if let prop_obj = properties_list[i] as? NSDictionary {
+                        if let address = prop_obj["address"] as? String {
+                            self.TableData.append(address)
+                        }
+                    }
+                }
+                self.tableview?.performSelectorOnMainThread(Selector("reloadData"), withObject: nil, waitUntilDone: true)
+            }
+        } catch {
+            
+        }
+    }
+    func extract_json(jsonData:NSData) {
+        let options: NSJSONReadingOptions = NSJSONReadingOptions()
+        var hasError = false
+        do {
+            let json: AnyObject? = try NSJSONSerialization.JSONObjectWithData(jsonData, options: options)
+            if let properties_list = json as? NSArray {
+                for (var i = 0; i < properties_list.count ; i++ ) {
+                    if let prop_obj = properties_list[i] as? NSDictionary {
+                        if let address = prop_obj["address"] as? String {
+                            TableData.append(address)
+                        }
+                    }
+                }
+            }
+        } catch {
+            
+        }
+    }
+    func do_table_refresh() {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableview?.reloadData()
+            return
+        })
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ PropsView7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PropsView7
+//
+//  Created by alex hawley on 7/18/16.
+//  Copyright © 2016 alex hawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
